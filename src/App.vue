ae9ba88085a3db188b85e763e0932a9fff895dfe
45555 <408385413@qq.com>
2020-11-06 09:26:47 +0800

    建立云库   建立项目   创建顶部导航栏

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <top-nav class="top-nav"></top-nav>
+</template>
+
+<script>
+import topNav from "./components/top-nav";
+
+export default {
+  name: "App",
+  components: {
+    topNav,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.top-nav {
+  width: 100%;
+  height: 4rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.33);
+}
+</style>
---
+++ src/components/top-nav.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="nav">
+    <div v-for="(nav, i) of navigations" :key="i">
+      <div>{{ nav }}</div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "top-nav",
+  data() {
+    return {
+      navigations: [
+        "首页",
+        "走进博物馆",
+        "陈列展览",
+        "参观指南",
+        "文创产品",
+        "网络购票",
+        "学术研究/出版物",
+        "社会活动",
+        "当地文化",
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.nav {
+  display: flex;
+}
+.nav > div > div {
+  width: 100%;
+}
+</style>
